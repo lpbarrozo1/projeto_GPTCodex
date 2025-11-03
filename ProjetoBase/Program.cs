41169a7b4ff9a06e88eb61c76b6e9311811d7aea
LPBarrozo <116750516+lpbarrozo1@users.noreply.github.com>
2025-11-03 13:11:28 -0400

    Update Program.cs

--- ProjetoBase/Program.cs
+++ ProjetoBase/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// registra MVC
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -22,27 +23,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-using System;
-
-namespace ProjetoBase;
-
-public static class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Bem-vindo ao projeto base em .NET!");
-
-        if (args.Length > 0)
-        {
-            Console.WriteLine("Argumentos fornecidos:");
-            foreach (var argument in args)
-            {
-                Console.WriteLine($" - {argument}");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Nenhum argumento foi passado para a aplicação.");
-        }
-    }
-}
